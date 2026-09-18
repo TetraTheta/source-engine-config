9a64f7c003bc72a41c3e37ab9693c66217db3480
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-09-18 13:51:23 +0900

    260918

--- entropy-zero-2/copy.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-@REM Copy SourceConfig
-set "source=%~dp0"
-set "dest=E:\Program Files\Steam\steamapps\common\EntropyZero2\entropyzero2"
-xcopy "%source%" "%dest%" /B /E /Y /EXCLUDE:%~dp0\xcopy_exclude.txt
-pause
